9001f49081d7e7ccd6615a907b939746c7f6b930
Mr.Jor <62343478+pomeluce@users.noreply.github.com>
2024-03-24 14:49:14 +0800

    调整了 dark theme 配色

--- src/rify/_styles/common/dark.ts
+++ src/rify/_styles/common/dark.ts
@@ -40,22 +40,16 @@ const base = {
   alphaTag: '0.2',
 
   // primary
-  primaryHover: '#7fe7c4',
-  primaryDefault: '#63e2b7',
-  primaryActive: '#5acea7',
-  primarySuppl: 'rgb(42, 148, 125)',
+  primaryHover: '#5faeff',
+  primaryDefault: '#409eff',
+  primaryActive: '#388be0',
+  primarySuppl: 'rgb(0, 102, 255)',
 
-  // info
-  infoHover: '#8acbec',
-  infoDefault: '#70c0e8',
-  infoActive: '#66afd3',
-  infoSuppl: 'rgb(56, 137, 197)',
-
-  // error
-  errorHover: '#e98b8b',
-  errorDefault: '#e88080',
-  errorActive: '#e57272',
-  errorSuppl: 'rgb(208, 58, 82)',
+  // success
+  successHover: '#7fe7c4',
+  successDefault: '#63e2b7',
+  successActive: '#5acea7',
+  successSuppl: 'rgb(42, 148, 125)',
 
   // warning
   warningHover: '#f5d599',
@@ -63,11 +57,17 @@ const base = {
   warningActive: '#e6c260',
   warningSuppl: 'rgb(240, 138, 0)',
 
-  // success
-  successHover: '#7fe7c4',
-  successDefault: '#63e2b7',
-  successActive: '#5acea7',
-  successSuppl: 'rgb(42, 148, 125)',
+  // error
+  errorHover: '#e98b8b',
+  errorDefault: '#e88080',
+  errorActive: '#e57272',
+  errorSuppl: 'rgb(208, 58, 82)',
+
+  // info
+  infoHover: '#a2a4a9',
+  infoDefault: '#909399',
+  infoActive: '#7f8187',
+  infoSuppl: 'rgb(52, 73, 94)',
 };
 
 const baseBackgroundRgb = rgba(base.neutralBase);
