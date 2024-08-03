e1074f97dc2b85a00d55a35a2302f7f7e9b3c6a0
Mr.Jor <62343478+pomeluce@users.noreply.github.com>
2024-08-03 11:15:54 +0800

    update: 更新 tauri 插件到 rc 版本

--- src/components/rify/loading/loading.tsx
+++ src/components/rify/loading/loading.tsx
@@ -9,8 +9,8 @@ export interface LoadingProps {
 const loading: React.FC<LoadingProps> = props => {
   return (
     <div
-      className="w-screen h-screen fixed inset-0 flex flex-col gap-2 justify-center items-center text-primary6 z-100"
-      style={{ backgroundColor: props.bgColor || 'rgba(var(--hue-bg-2), 0.45)', color: props.color }}
+      className="w-screen h-screen fixed inset-0 flex flex-col gap-2 justify-center items-center text-primary6 z-100 bg-backdrop2/45"
+      style={{ backgroundColor: props.bgColor, color: props.color }}
     >
       <div className="relative w-10 h-10" role="img" aria-label="loading">
         <div className="absolute w-full h-full">
