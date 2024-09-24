7eff097d378418e6e139982c8077e7ff05d02245
Mr.Jor <62343478+pomeluce@users.noreply.github.com>
2024-09-24 21:17:50 +0800

    update: 更新了 tauri 主题插件, 调整了导航栏布局

--- tauri/layouts/titlebar/menu-group.tsx
+++ tauri/layouts/titlebar/menu-group.tsx
@@ -1,15 +1,9 @@
-import { ThemePopup } from '@common/components';
 import { About, Pushpin } from '@tauri/components';
 
 const menuGroup: React.FC<{}> = () => {
   return (
     <main className="flex items-center gap-3 ml-auto text-word2">
       <Pushpin />
-      <ThemePopup
-        content={theme => (
-          <button className="flex justify-center items-center">{(theme === 'system' ? IconRiComputerFill : theme === 'light' ? IconRiSunFill : IconRiMoonClearFill)({})}</button>
-        )}
-      />
       <About />
     </main>
   );
