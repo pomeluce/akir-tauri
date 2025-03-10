f56b69f62c243be40ec1f54841af85fcf5a5465f
Mr.Jor <62343478+pomeluce@users.noreply.github.com>
2025-03-10 10:22:33 +0800

    fix: Fixed the issue of waterfall flow truncation on the settings page

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -3,7 +3,7 @@ import { General, Keymap, UI } from './-settings';
 export const Route = createFileRoute('/settings/')({
   component: () => {
     return (
-      <main className="columns-1 lg:columns-2 -mt-3 *:mt-3 gap-3 px-5 overflow-scroll">
+      <main className="columns-auto lg:columns-2 -mt-3 *:mt-3 *:break-inside-avoid gap-3 px-5 overflow-scroll">
         <General />
         <UI />
         <Keymap />
